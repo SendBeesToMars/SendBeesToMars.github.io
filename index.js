601ffdfa894f359f7e4532a6fae87e5fdfb58b33
Winmillion <datum1337@gmail.com>
2023-04-10 02:51:54 +0100

    looks like words are back on the menu boys

--- index.js
+++ index.js
@@ -4,22 +4,7 @@ onclick = function () {
 
 let background = document.body.style;
 let content = document.getElementById("content");
-let gyroscope = new Gyroscope({
-    frequency: 60
-});
 let tilt_flag = false;
-
-// gyroscope.addEventListener("reading", (e) => {
-//     if (gyroscope.y >= 8) {
-//         background.backgroundColor = "yellow";
-//         // get_word();
-//     }
-//     if (gyroscope.y <= -8) {
-//         background.backgroundColor = "lime";
-//         // get_word();
-//     }
-// });
-// gyroscope.start();
 
 const options = {
     method: 'GET',
@@ -27,39 +12,46 @@ const options = {
 };
 
 function get_word() {
-    // fetch('https://random-word-api.herokuapp.com/word', options)
-    //     .then(response => response.json())
-    //     .then(response => {
-    //         //console.log(response);
-    //         set_word(response);
-    //     })
-    //     .catch(err => console.error(err));
+    fetch('https://random-word-api.herokuapp.com/word', options)
+        .then(response => response.json())
+        .then(response => {
+            //console.log(response);
+            set_word(response);
+        })
+        .catch(err => console.error(err));
 }
 
 function set_word(word) {
     content.textContent = word;
 }
 
-// get_word();
+get_word();
 
 function handle_orientation(event) {
     const tilt = Math.round(event.gamma) + 90;
     const beta = Math.round(event.beta);
-    set_word(tilt + "______" + beta);
 
     if (beta < 30 && beta > -30) {
         if (((tilt > 145 && tilt <= 180) || (tilt >= 0 && tilt < 35))) {
             background.backgroundColor = "CornflowerBlue";
             tilt_flag = false;
         } else {
-            background.backgroundColor = "Tomato";
+            if (!tilt_flag) {
+                background.backgroundColor = "Tomato";
+                tilt_flag = true;
+                get_word();
+            }
         }
     } else {
         if (((tilt > 130 && tilt <= 180) || (tilt >= 0 && tilt < 50))) {
             background.backgroundColor = "CornflowerBlue";
             tilt_flag = false;
         } else {
-            background.backgroundColor = "YellowGreen";
+            if (!tilt_flag) {
+                background.backgroundColor = "YellowGreen";
+                tilt_flag = true;
+                get_word();
+            }
         }
     }
 }
